ab594cd017fa0eb51e34f537f35354a2e810fd26
Dave Snyder <87585510+MKEDataGuy@users.noreply.github.com>
2021-08-23 19:20:19 -0500

    Start Account Management

---
+++ src/HeadLight/Security/Roles/Database Roles/WebApplication.sql
@@ -0,0 +1,29 @@
+﻿CREATE ROLE [WebApplication];
+GO
+
+GRANT EXECUTE ON [dbo].[CreateLogin] TO [WebApplication];
+GO
+
+GRANT EXECUTE ON [dbo].[CreateUser] TO [WebApplication];
+GO
+
+GRANT EXECUTE ON [dbo].[DeleteLogin] TO [WebApplication];
+GO
+
+GRANT EXECUTE ON [dbo].[DeleteUser] TO [WebApplication];
+GO
+
+GRANT EXECUTE ON [dbo].[RetrieveLoginByUserId] TO [WebApplication];
+GO
+
+GRANT EXECUTE ON [dbo].[RetrieveUserByEmail] TO [WebApplication];
+GO
+
+GRANT EXECUTE ON [dbo].[RetrieveUserByLogin] TO [WebApplication];
+GO
+
+GRANT EXECUTE ON [dbo].[RetrieveUserByUserId] TO [WebApplication];
+GO
+
+GRANT EXECUTE ON [dbo].[UpdateUser] TO [WebApplication];
+GO
